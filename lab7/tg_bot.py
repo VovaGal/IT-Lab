cf380734947e1bb19809874ef2c1da2a96da70e1
VovaGal <125148753+VovaGal@users.noreply.github.com>
2023-03-09 02:36:04 +0300

    Update tg_bot.py

--- lab7/tg_bot.py
+++ lab7/tg_bot.py
@@ -10,8 +10,8 @@ conn = psycopg2.connect(database="timestable_db",
                         host="localhost",
                         port="5433")
 cursor = conn.cursor()
-psql_select_timetable1 = "select day, string_agg(table_column, '\n') as table_row from (select day, timetable_week1.subject ||' | '|| room_numb ||' | '|| start_time ||'-'|| finish_time ||' | '|| full_name as table_column from timetable_week1, teacher where teacher.subject = timetable_week1.subject order by start_time)timetable_week1 group by 1 order by case when day = 'Monday' then 1 when day = 'Tuesday' then 2 when day = 'Wednesday' then 3 when day = 'Thursday' then 4 else 5 end;"
-psql_select_timetable2 = "select day, string_agg(table_column, '\n') as table_row from (select day, timetable_week2.subject ||' | '|| room_numb ||' | '|| start_time ||'-'|| finish_time ||' | '|| full_name as table_column from timetable_week2, teacher where teacher.subject = timetable_week2.subject order by start_time)timetable_week2 group by 1 order by case when day = 'Monday' then 1 when day = 'Tuesday' then 2 when day = 'Wednesday' then 3 when day = 'Thursday' then 4 else 5 end;"
+psql_select_timetable1 = "select day, string_agg(table_column, '\n\n') as table_row from (select day, timetable_week1.subject ||' | '|| room_numb ||' | '|| start_time ||'-'|| finish_time ||' | '|| full_name as table_column from timetable_week1, teacher where teacher.subject = timetable_week1.subject order by start_time)timetable_week1 group by 1 order by case when day = 'Monday' then 1 when day = 'Tuesday' then 2 when day = 'Wednesday' then 3 when day = 'Thursday' then 4 else 5 end;"
+psql_select_timetable2 = "select day, string_agg(table_column, '\n\n') as table_row from (select day, timetable_week2.subject ||' | '|| room_numb ||' | '|| start_time ||'-'|| finish_time ||' | '|| full_name as table_column from timetable_week2, teacher where teacher.subject = timetable_week2.subject order by start_time)timetable_week2 group by 1 order by case when day = 'Monday' then 1 when day = 'Tuesday' then 2 when day = 'Wednesday' then 3 when day = 'Thursday' then 4 else 5 end;"
 
 
 today = date.today()
@@ -90,7 +90,7 @@ def start_message(message):
 def answer(message):
     if message.text.lower() == 'monday':
         cursor.execute(
-            "select day, string_agg(table_column, '\n') as table_row from (select day, {}.subject ||' | '|| room_numb ||' | '|| start_time ||'-'|| finish_time ||' | '|| full_name as table_column from {}, teacher where day ='Monday' and teacher.subject = {}.subject order by start_time) {} group by 1;".format(
+            "select day, string_agg(table_column, '\n\n') as table_row from (select day, {}.subject ||' | '|| room_numb ||' | '|| start_time ||'-'|| finish_time ||' | '|| full_name as table_column from {}, teacher where day ='Monday' and teacher.subject = {}.subject order by start_time) {} group by 1;".format(
                 this_week, this_week, this_week, this_week))
         tb1_records = cursor.fetchall()
         for row in tb1_records:
@@ -98,7 +98,7 @@ def answer(message):
             bot.send_message(message.chat.id, m1_display)
     elif message.text.lower() == 'tuesday':
         cursor.execute(
-            "select day, string_agg(table_column, '\n') as table_row from (select day, {}.subject ||' | '|| room_numb ||' | '|| start_time ||'-'|| finish_time ||' | '|| full_name as table_column from {}, teacher where day ='Tuesday' and teacher.subject = {}.subject order by start_time) {} group by 1;".format(
+            "select day, string_agg(table_column, '\n\n') as table_row from (select day, {}.subject ||' | '|| room_numb ||' | '|| start_time ||'-'|| finish_time ||' | '|| full_name as table_column from {}, teacher where day ='Tuesday' and teacher.subject = {}.subject order by start_time) {} group by 1;".format(
                 this_week, this_week, this_week, this_week))
         tb1_records = cursor.fetchall()
         for row in tb1_records:
@@ -106,7 +106,7 @@ def answer(message):
             bot.send_message(message.chat.id, m1_display)
     elif message.text.lower() == 'wednesday':
         cursor.execute(
-            "select day, string_agg(table_column, '\n') as table_row from (select day, {}.subject ||' | '|| room_numb ||' | '|| start_time ||'-'|| finish_time ||' | '|| full_name as table_column from {}, teacher where day ='Wednesday' and teacher.subject = {}.subject order by start_time) {} group by 1;".format(
+            "select day, string_agg(table_column, '\n\n') as table_row from (select day, {}.subject ||' | '|| room_numb ||' | '|| start_time ||'-'|| finish_time ||' | '|| full_name as table_column from {}, teacher where day ='Wednesday' and teacher.subject = {}.subject order by start_time) {} group by 1;".format(
                 this_week, this_week, this_week, this_week))
         tb1_records = cursor.fetchall()
         for row in tb1_records:
@@ -114,7 +114,7 @@ def answer(message):
             bot.send_message(message.chat.id, m1_display)
     elif message.text.lower() == 'thursday':
         cursor.execute(
-            "select day, string_agg(table_column, '\n') as table_row from (select day, {}.subject ||' | '|| room_numb ||' | '|| start_time ||'-'|| finish_time ||' | '|| full_name as table_column from {}, teacher where day ='Thursday' and teacher.subject = {}.subject order by start_time) {} group by 1;".format(
+            "select day, string_agg(table_column, '\n\n') as table_row from (select day, {}.subject ||' | '|| room_numb ||' | '|| start_time ||'-'|| finish_time ||' | '|| full_name as table_column from {}, teacher where day ='Thursday' and teacher.subject = {}.subject order by start_time) {} group by 1;".format(
                 this_week, this_week, this_week, this_week))
         tb1_records = cursor.fetchall()
         for row in tb1_records:
@@ -122,7 +122,7 @@ def answer(message):
             bot.send_message(message.chat.id, m1_display)
     elif message.text.lower() == 'friday':
         cursor.execute(
-            "select day, string_agg(table_column, '\n') as table_row from (select day, {}.subject ||' | '|| room_numb ||' | '|| start_time ||'-'|| finish_time ||' | '|| full_name as table_column from {}, teacher where day ='Friday' and teacher.subject = {}.subject order by start_time) {} group by 1;".format(
+            "select day, string_agg(table_column, '\n\n') as table_row from (select day, {}.subject ||' | '|| room_numb ||' | '|| start_time ||'-'|| finish_time ||' | '|| full_name as table_column from {}, teacher where day ='Friday' and teacher.subject = {}.subject order by start_time) {} group by 1;".format(
                 this_week, this_week, this_week, this_week))
         tb1_records = cursor.fetchall()
         for row in tb1_records:
